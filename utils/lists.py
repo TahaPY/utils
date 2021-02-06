97c4af512b5f0684f2e6aa110e2b12c7bd6e3861
Alireza <alirezarabie793@gmail.com>
2021-02-06 16:19:11 +0330

    Added chunk function and tests

--- utils/lists.py
+++ utils/lists.py
@@ -1,4 +1,9 @@
 def chunks(arr, n):
-    """Yield successive n-sized chunks from lst."""
+    """
+    Yield successive n-sized chunks from arr.
+    :param arr
+    :param n
+    :return generator
+    """
     for i in range(0, len(arr), n):
         yield arr[i:i + n]
