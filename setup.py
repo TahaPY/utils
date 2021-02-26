4e794cf8a4b95015a051a6e197473ee6ea6946b5
Alireza <alirezarabie793@gmail.com>
2021-02-26 18:10:43 +0330

    Added a new function to lists and added url utility

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='utils-aazerra',
-    version='0.0.2',
+    version='0.0.3',
     packages=['utils'],
     url='https://github.com/Aazerra/utils',
     license='MIT',
--- utils/lists.py
+++ utils/lists.py
@@ -1,3 +1,6 @@
+from itertools import islice
+
+
 def chunks(arr, n):
     """
     Yield successive n-sized chunks from arr.
@@ -7,3 +10,13 @@ def chunks(arr, n):
     """
     for i in range(0, len(arr), n):
         yield arr[i:i + n]
+
+
+def take(arr, n):
+    """
+    Returns n element of the arr.
+    :param arr:
+    :param n:
+    :return: list of n element
+    """
+    return list(islice(arr, n))
